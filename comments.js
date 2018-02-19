3616947b58c97951cdc8860029768feda738fe9f
Grzegorz Kluka <g.kluka@wp.pl>
2018-02-20 00:48:21 +0100

    Fixed bugs

--- comments.js
+++ comments.js
@@ -1,73 +1,85 @@
-import {ADD_COMMENT} from './actions';
-import {REMOVE_COMMENT} from './actions';
-import {EDIT_COMMENT} from './actions';
-import {THUMB_UP_COMMENT} from './actions';
-import {THUMB_DOWN_COMMENT} from './actions';
+import { ADD_COMMENT } from './actions';
+import { REMOVE_COMMENT } from './actions';
+import { EDIT_COMMENT } from './actions';
+import { THUMB_UP_COMMENT } from './actions';
+import { THUMB_DOWN_COMMENT } from './actions';
 
 const initialState = {
     comments: [],
-    users: []
+    users: [],
 };
 
-export const comments = (state = [], action) => {
+export default function comments(state = [], action) {
     switch (action.type) {
         case ADD_COMMENT:
-            return [{
-                id: action.id,
-                text: action.text,
-                votesUp: 0,
-                votesDown: 0
-            }, ...state.comments];
-            break;
+            return [
+                {
+                    id: action.id,
+                    text: action.text,
+                    votesUp: 0,
+                    votesDown: 0,
+                },
+                ...state.comments,
+            ];
 
-        case EDIT_COMMENT:
-            const check = state.comments.find(comment => (comment.id === action.id));
+        case EDIT_COMMENT: {
+            const check = state.comments.find(
+                comment => comment.id === action.id,
+            );
             if (check) {
                 const index = state.comments.indexOf(check);
                 return [
-                   ...state.comments.slice(0, index),
-                   {...state.comments[index], text: action.text},
-                   ...state.comments.slice(index+1, state.comments.length)
+                    ...state.comments.slice(0, index),
+                    { ...state.comments[index], text: action.text },
+                    ...state.comments.slice(index + 1, state.comments.length),
                 ];
             } else {
                 return state;
             }
-            break;
+        }
 
-        case THUMB_UP_COMMENT:
-            const check = state.comments.find(comment => (comment.id === action.id));
+        case THUMB_UP_COMMENT: {
+            const check = state.comments.find(
+                comment => comment.id === action.id,
+            );
             if (check) {
                 const index = state.comments.indexOf(check);
                 return [
-                   ...state.comments.slice(0, index),
-                   {...state.comments[index], votesUp: state.comments[index].votesUp + 1},
-                   ...state.comments.slice(index+1, state.comments.length)
+                    ...state.comments.slice(0, index),
+                    {
+                        ...state.comments[index],
+                        votesUp: state.comments[index].votesUp + 1,
+                    },
+                    ...state.comments.slice(index + 1, state.comments.length),
                 ];
             } else {
                 return state;
             }
-            break;
+        }
 
-        case THUMB_DOWN_COMMENT:
-            const check = state.comments.find(comment => (comment.id === action.id));
+        case THUMB_DOWN_COMMENT: {
+            const check = state.comments.find(
+                comment => comment.id === action.id,
+            );
             if (check) {
                 const index = state.comments.indexOf(check);
                 return [
-                   ...state.comments.slice(0, index),
-                   {...state.comments[index], votesDown: state.comments[index].votesDown + 1},
-                   ...state.comments.slice(index+1, state.comments.length)
+                    ...state.comments.slice(0, index),
+                    {
+                        ...state.comments[index],
+                        votesDown: state.comments[index].votesDown + 1,
+                    },
+                    ...state.comments.slice(index + 1, state.comments.length),
                 ];
             } else {
                 return state;
             }
-            break;
+        }
 
         case REMOVE_COMMENT:
             return state.comments.filter(comment => comment.id !== action.id);
-            break;
 
         default:
             return state;
     }
-    
 }
